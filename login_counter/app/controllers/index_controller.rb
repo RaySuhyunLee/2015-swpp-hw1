00cb62dd6f4aa6349f9b6898814ede0479770922
RaySuhyunLee <raysuhyunlee@gmail.com>
2015-03-29 22:06:02 +0900

    [FIX] Improved error message viewer by using global variable instead of sending get request with parameter

--- login_counter/app/controllers/index_controller.rb
+++ login_counter/app/controllers/index_controller.rb
@@ -1,14 +1,13 @@
 class IndexController < ApplicationController
 	def index
-		error_code = params[:error_code]
-		case error_code
-		when "-1"
+		case $error_code
+		when -1
 			@message = "The user name should be 5~20 characters long. Please try again."
-		when "-2"
+		when -2
 			@message = "The password should be 8~20 characters long. Please try again."
-		when "-3"
+		when -3
 			@message = "This user name already exists. Please try again."
-		when "-4"
+		when -4
 			@message = "Invalid username and password combination. Please try again."
 		else
 			@message = "Please enter your credentials below"
@@ -22,16 +21,20 @@ class IndexController < ApplicationController
 		@user = UserAccount.find_by(username: @id, pass: @pass)
 		respond_to do |format|
 			if @id.length < 5 or @id.length > 20
-				format.html { redirect_to :action => "index", :error_code => -1 }
+				$error_code = -1
+				format.html { redirect_to :action => "index" }
 				format.json { render :json => { :error_code => -1 } }
 			elsif @pass.length < 8 or @pass.length > 20
-				format.html { redirect_to :action => "index", :error_code => -2 }
+				$error_code = -2
+				format.html { redirect_to :action => "index" }
 				format.json { render :json => { :error_code => -2 } }
 
 			elsif @user.nil?
-				format.html { redirect_to :action => "index", :error_code => -4 }
+				$error_code = -4
+				format.html { redirect_to :action => "index" }
 				format.json { render :json => { :error_code => -4 } }
 			else
+				$error_code = nil
 				@user.count += 1
 				@user.save
 
@@ -47,19 +50,23 @@ class IndexController < ApplicationController
 
 		respond_to do |format|
 			if @id.length < 5 or @id.length > 20
-				format.html { redirect_to :action => "index", :error_code => -1 }
+				$error_code = -1
+				format.html { redirect_to :action => "index" }
 				format.json { render :json => { :error_code => -1 } }
 			elsif @pass.length < 8 or @pass.length > 20
-				format.html { redirect_to :action => "index", :error_code => -2 }
+				$error_code = -2
+				format.html { redirect_to :action => "index" }
 				format.json { render :json => { :error_code => -2 } }
 			elsif not UserAccount.find_by(username: @id).nil?
+				$error_code = -3
 				msg = { :error_code => -3 }
-				format.html { redirect_to :action => "index", :error_code => -3 }
+				format.html { redirect_to :action => "index" }
 				format.json { render :json => msg }
 			else
+				$error_code = nil
 				user = UserAccount.create(username: @id, pass: @pass, count: 1)
 				msg = { :user_name => @id, :login_count => user.count }
-				format.html { redirect_to :action => "index" }
+				format.html { redirect_to :action => "login" }
 				format.json { render :json => msg } 
 			end
 		end
